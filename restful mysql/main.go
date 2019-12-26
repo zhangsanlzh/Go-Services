ebe6b4b5b1a041a86886be8b239e857fd60fc662
Luskyle <844804539@qq.com>
2019-12-26 22:45:09 +0800

    update

--- restful mysql/main.go
+++ restful mysql/main.go
@@ -9,6 +9,5 @@ import (
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/api/select/tbluser", tbluserHandler).Methods("GET")
-	r.HandleFunc("/api/select/tbluser", tbluserHandler).Methods("GET")
 	log.Fatal(http.ListenAndServe(":8080", r))
 }
